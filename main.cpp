950dfab77c4bf5620fbc16a8a8b4f2c50551a9c3
RaKalik963 <andrejmahot646@gmail.com>
2024-05-18 16:07:32 +0300

    lab_ex3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 int main()
 {
+	const int size = 5;
+	cout << "enter side 1 ";
+	int side1;
+	cin >> side1;
+	cout << "enter side 2 ";
+	int side2;
+	cin >> side2;
+	cout << "enter side 3 ";
+	int side3;
+	cin >> side3;
+	cout << "enter side 4 ";
+	int side4;
+	cin >> side4;
+	cout << "enter side 5 ";
+	int side5;
+	cin >> side5;
 
+	int arr[size] = {side1,side2,side3,side4,side5};
+
+	float sum = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "perimetr= " << sum << endl;
 	return 0;
 	system("pause");
 }
