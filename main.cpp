ff6b14339f4faa2c5102c5fba3e613a59c822c93
RaKalik963 <andrejmahot646@gmail.com>
2024-05-18 17:47:04 +0300

    2352832

--- main.cpp
+++ main.cpp
@@ -5,12 +5,23 @@ using namespace std;
 
 int main()
 {        
-    const int size = 10;
-    double arr[size];
+    const int size = 20;
+    srand(time(0));
 
-    cout << "enter num" << endl;
+    int arr[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = rand() % 25 - 10;
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    int neg = 0;
     for (int i = 0; i < size; ++i) {
-        cin >> arr[i];
+        if (arr[i] < 0) {
+            neg += arr[i];
+        }
     }
 	return 0;
 	system("pause");
