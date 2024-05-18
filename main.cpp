457114384ae6ed36811c03abc93fd076092316cb
RaKalik963 <andrejmahot646@gmail.com>
2024-05-18 15:55:15 +0300

    lab_ex1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,34 @@ using namespace std;
 
 int main()
 {
+	const int size = 6;
+	cout << "enter the profit for the first month";
+	int month1;
+	cin >> month1;
+	cout << "enter the profit for the second month";
+	int month2;
+	cin >> month2;
+	cout << "enter the profit for the third month";
+	int month3;
+	cin >> month3;
+	cout << "enter the profit for the fourth month";
+	int month4;
+	cin >> month4;
+	cout << "enter the profit for the fifth month";
+	int month5;
+	cin >> month5;
+	cout << "enter the profit for the six month";
+	int month6;
+	cin >> month6;
 
+	int arr[size] = {month1,month2,month3,month4,month5,month6};
+
+	float sum = 0;
+	for (size_t i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+	cout << "Sum: " << sum << endl;
 	return 0;
 	system("pause");
 }
