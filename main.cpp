9f9ba8454dabb66ff070530b375e88896dec1e63
RaKalik963 <andrejmahot646@gmail.com>
2024-05-18 16:00:10 +0300

    12

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main()
 {
 
+    cout << "reverse: " << endl;
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 	return 0;
 	system("pause");
 }
