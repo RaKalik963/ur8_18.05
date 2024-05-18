04c2714e00798b5e35f0ce963bc97f1e72d279da
RaKalik963 <andrejmahot646@gmail.com>
2024-05-18 16:03:37 +0300

    lab_ex2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main()
 {
+	const int size = 6;
 
-    cout << "reverse: " << endl;
-    for (int i = n - 1; i >= 0; i--) {
+	int arr[size] = {1,2,3,4,5,6};
+
+    cout << "reverse: ";
+    for (int i = size - 1; i >= 0; i--) {
         cout << arr[i] << " ";
     }
     cout << endl;
